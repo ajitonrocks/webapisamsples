054748f6218b8f5082ee17ab796b4a55f45e2490
shekhawatajit <shekhawatajit@hotmail.com>
2022-07-23 08:47:25 +0000

    swagger added

--- src/app.js
+++ src/app.js
@@ -12,5 +12,12 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+app.use(
+  '/swagger',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
